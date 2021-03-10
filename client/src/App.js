2b82210eb99ab5945abb287deb5006af61ed3363
Roopak Chugh <chughroopak@gmail.com>
2021-03-10 23:51:11 +0530

    added authentication and basic design

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import store from "./store";
+import NavBar from "./components/layout/NavBar";
+import Login from "./components/auth/Login";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+//check for token
+if (localStorage.getItem("jwtToken")) {
+  //set auth token header auth
+  setAuthToken(localStorage.getItem("jwtToken"));
+  //decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //logout user
+    store.dispatch(logoutUser());
+    //Redirect to login
+    window.location.href = "/login";
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <NavBar />
+          <div className="container">
+            <Route exact path="/login" component={Login}></Route>
+          </div>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
