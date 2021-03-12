65bf61cdfbee6c795c03c1282c8e789b348a40c6
Roopak Chugh <chughroopak@gmail.com>
2021-03-12 09:43:43 +0530

    added heroku configs

--- server.js
+++ server.js
@@ -28,7 +28,16 @@ require("./config/passport")(passport);
 app.use("/api/users/", users);
 app.use("/api/posts", posts);
 
-const port = 5000;
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
