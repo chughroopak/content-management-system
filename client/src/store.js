ea514cbef24a783366693f9278c21cb6d4ade1ac
Roopak Chugh <chughroopak@gmail.com>
2021-03-12 10:02:58 +0530

    removed devtools for redux to allow heroku deploy

--- client/src/store.js
+++ client/src/store.js
@@ -1,21 +1,14 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
 const middleware = [thunk];
 
-const devTools =
-  // process.env.NODE_ENV !== "production"
-  //   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
-  //     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-  //   :
-  null;
-
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(...middleware), devTools)
+  applyMiddleware(...middleware)
 );
 
 export default store;
